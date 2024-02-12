488709d6b96593d22f0df5ccc12c9da04cce1763
zrdominguez <zechariahd@gmail.com>
2024-02-12 09:44:46 -0600

    complete problem 19

--- problems/19-greater-callback-value.js
+++ problems/19-greater-callback-value.js
@@ -19,7 +19,9 @@ console.log(greaterCallbackValue(9, Math.sqrt, doubler));   // 18
 *******************************************************************************/
 
 function greaterCallbackValue(val, cb1, cb2) {
-  // Your code here 
+  let result1 = cb1(val);
+  let result2 = cb2(val);
+  return result1 > result2 ? result1 : result2;
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
