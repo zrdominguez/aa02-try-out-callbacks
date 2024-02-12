b3a90343c91eb855de5ade9038d72ebc26d000bc
zrdominguez <zechariahd@gmail.com>
2024-02-12 10:12:02 -0600

    completed problem 22

--- problems/22-first-index.js
+++ problems/22-first-index.js
@@ -23,7 +23,8 @@ console.log(result3); // -1
 *******************************************************************************/
 
 function firstIndex(array, cb) {
-  // Your code here 
+   let check = array.find(el => cb(el))
+   return array.indexOf(check);
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
